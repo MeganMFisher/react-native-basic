491089845b02aafd2f61c7af60c8d2ced9021865
Megan Fisher <meganfisher@Stacys-Work-MBP.localdomain>
2018-03-16 15:00:38 -0600

    button press

--- Component1.js
+++ Component1.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
 
 export default class Component1 extends Component {
     render() {
         return (
+            <View>
             <Text style={styles.text}>{this.props.name}</Text>
+            <Button 
+                title='Press Me!' 
+                onPress={() => Alert.alert('button press!')}
+                color='white'
+                />
+            </View>
         )
     }
 }
